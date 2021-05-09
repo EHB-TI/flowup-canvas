b3e3ac795f8ab25b14350542374b527bddaf6156
Abdirahman <rizetracker@gmail.com>
2021-05-09 07:14:15 +0200

    Integrated MasterUUID into RabbitMQ reciever

--- rabbitmq/recieve.js
+++ rabbitmq/recieve.js
@@ -4,6 +4,7 @@ const {UserHelper} = require('../Helpers/userHelper.js');
 const {EventHelper} = require("../Helpers/eventHelper.js");
 const {User} = require("../DTO/user.js");
 const {Event} = require("../DTO/event.js");
+const {UUIDHelper} = require("../MasterUUID/uuid_helper.js");
 
 
 require('dotenv').config();
@@ -12,6 +13,8 @@ require('dotenv').config();
 const parser = new xml2js.Parser( /* options */ );
 
 (async function() {
+
+    const source = "Canvas";
     
     try {
         const connection = await amqp.connect(process.env.AMQP_URL);
@@ -41,6 +44,11 @@ const parser = new xml2js.Parser( /* options */ );
                 let UUID_info = result[type].header;
 
                 let method = UUID_info[0].method[0];
+
+                const uuidhelper = new UUIDHelper();
+
+                // first talk to the DB , this is the same regardless of which type (user or event)
+                uuidhelper.handleDb(UUID_info[0].UUID[0],source,type.charAt(0).toUpperCase() + type.slice(1),parseInt(UUID_info[0].version[0]),method);
  
                 if (msg.fields.routingKey === "user"){
                     let user = new User(Body_info[0].firstname[0],Body_info[0].lastname[0],Body_info[0].email[0],UUID_info[0].UUID[0]);
@@ -51,6 +59,8 @@ const parser = new xml2js.Parser( /* options */ );
                 }
 
                 else if (msg.fields.routingKey === "events"){
+                    console.log(Body_info[0].name[0]);
+                    console.log(Body_info[0].description[0]);
                     let event = new Event(Body_info[0].name[0],Body_info[0].description[0],UUID_info[0].UUID[0]);
                     let status = EventHelper.handle(event,method);
                     if (status === 200){
