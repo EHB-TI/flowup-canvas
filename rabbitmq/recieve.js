ef15ea974db75bb49bad011792e33e1653511ee8
Abdirahman <rizetracker@gmail.com>
2021-05-12 12:38:00 +0200

    durable queue in reciever + eventsubscribe integration

--- rabbitmq/recieve.js
+++ rabbitmq/recieve.js
@@ -5,6 +5,8 @@ const {EventHelper} = require("../Helpers/eventHelper.js");
 const {User} = require("../DTO/user.js");
 const {Event} = require("../DTO/event.js");
 const {UUIDHelper} = require("../MasterUUID/uuid_helper.js");
+const {EventSubscription} = require("../DTO/eventsubscription.js");
+const {EventSubscribeHelper} = require("../Helpers/eventsubscribeHelper.js");
 
 
 require('dotenv').config();
@@ -20,22 +22,23 @@ const parser = new xml2js.Parser( /* options */ );
         const connection = await amqp.connect(process.env.AMQP_URL);
         const channel = await connection.createChannel();
         const exchange = "direct_logs";
-        const types = ["user", "events"];
+        const types = ["user", "event" , "eventsubscribe"];
+        const queue = "Canvas"
 
         channel.assertExchange(exchange, 'direct', {
             durable: false
           });
 
-        const q = await channel.assertQueue('', {
-            exclusive: true
+        await channel.assertQueue(queue, {
+            durable: true
         });
 
         for(let type of types){
-            channel.bindQueue(q.queue, exchange, type);
+            channel.bindQueue(queue, exchange, type);
         }
         
         channel.prefetch(1);
-        channel.consume(q.queue, (msg) => {
+        channel.consume(queue, (msg) => {
 
             parser.parseStringPromise(msg.content.toString()).then(result => {
 
@@ -48,28 +51,32 @@ const parser = new xml2js.Parser( /* options */ );
                 const uuidhelper = new UUIDHelper();
 
                 // first talk to the DB , this is the same regardless of which type (user or event)
-                uuidhelper.handleDb(UUID_info[0].UUID[0],source,type.charAt(0).toUpperCase() + type.slice(1),parseInt(UUID_info[0].version[0]),method);
+                //uuidhelper.handleDb(UUID_info[0].UUID[0],source,type.charAt(0).toUpperCase() + type.slice(1),parseInt(UUID_info[0].version[0]),method);
  
+                // if type is user => process the user
                 if (msg.fields.routingKey === "user"){
                     let user = new User(Body_info[0].firstname[0],Body_info[0].lastname[0],Body_info[0].email[0],UUID_info[0].UUID[0]);
                     let status = UserHelper.handle(user,method);
-                    if (status === 200){
-                        channel.ack(msg);
-                    }
+                    console.log(`Status: ${status}`);
                 }
 
-                else if (msg.fields.routingKey === "events"){
-                    console.log(Body_info[0].name[0]);
-                    console.log(Body_info[0].description[0]);
+                // if type is user => process the event
+                else if (msg.fields.routingKey === "event"){
                     let event = new Event(Body_info[0].name[0],Body_info[0].description[0],UUID_info[0].UUID[0]);
                     let status = EventHelper.handle(event,method);
-                    if (status === 200){
-                        channel.ack(msg);
-                    }
+                    console.log(`Status: ${status}`);
                 }
+
+                // if type eventsubscribe => add user to the event
+                else if (msg.fields.routingKey === "eventsubscribe"){
+                    let eventsubscription = new EventSubscription(Body_info[0].eventUUID[0],Body_info[0].attendeeUUID[0]); 
+                    EventSubscribeHelper.handle(eventsubscription,method);     
+                }
+
             }).catch(err => {
                 console.log(err);
             });
+            channel.ack(msg);
             console.log(" [x] Received %s: %s",msg.fields.routingKey, msg.content.toString());
         }, {
             noAck: false
