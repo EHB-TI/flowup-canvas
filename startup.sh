af5816e2f2979a34cb7168c00015283083a0c502
Abdirahman <rizetracker@gmail.com>
2021-06-02 20:18:00 +0200

    test3

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 echo "IP2021" | sudo -S docker stop heartbeat
 echo "IP2021" | sudo -S docker stop receiver
+
 echo "IP2021" | sudo -S docker-compose down -v
 echo "IP2021" | sudo -S docker-compose up  --build
