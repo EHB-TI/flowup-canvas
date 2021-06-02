ce4664ba2332fdc9671c6045ddbea470c117f971
Abdirahman <rizetracker@gmail.com>
2021-06-02 20:55:56 +0200

    change in startup

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ echo "IP2021" | sudo -S docker stop heartbeat
 echo "IP2021" | sudo -S docker stop receiver
 
 echo "IP2021" | sudo -S docker-compose down -v
-echo "IP2021" | sudo -S docker-compose up  --build
+echo "IP2021" | sudo -S docker-compose up -d --build
