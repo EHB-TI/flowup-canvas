8a9d0ec8b3415687b0609e9dcb66338dce18a61e
Abdirahman <rizetracker@gmail.com>
2021-06-02 16:25:13 +0200

    small fix

--- API/axios_config.js
+++ API/axios_config.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
 const querystring = require('querystring');
 require('dotenv').config();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 // axios default configuration that will be applied for each request 
 
-axios.defaults.headers.common["Authorization"] = `Bearer ${process.env.API_token}`
+axios.defaults.headers.common["Authorization"] = `Bearer ${process.env.API_TOKEN}`
 axios.defaults.headers.common["Content-type"] = "application/json"
 
 axios.defaults.baseURL = process.env.BASEURL;
