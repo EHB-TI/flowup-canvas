c9f9b66fbdc79120d305e387f802607a5e1bb8bc
Abdirahman <rizetracker@gmail.com>
2021-06-02 20:11:51 +0200

    update startup script

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
+echo "IP2021" | sudo -S docker stop heartbeat
+echo "IP2021" | sudo -S docker stop receiver
 echo "IP2021" | sudo -S docker-compose down -v
 echo "IP2021" | sudo -S docker-compose up  --build
