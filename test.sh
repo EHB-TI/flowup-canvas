2f1741215276294d5780ddfdff596cfd5888c299
AbdirahmanM <39442337+AbdirahmanM@users.noreply.github.com>
2021-06-03 22:58:58 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-cp /home/canvas/.env.example  /home/canvas/actions-runner/_work/flowup-canvas/flowup-canvas/.env
+echo "IP2021" | sudo -S cp /home/canvas/.env.example  /home/canvas/actions-runner/_work/flowup-canvas/flowup-canvas/.env
