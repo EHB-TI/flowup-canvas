f6d7c39c34381315252e58a20728041e87e418b0
Abdirahman <rizetracker@gmail.com>
2021-06-04 00:16:48 +0200

    test1

--- DB/db.js
+++ DB/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const pool  = mysql.createPool({
   host: process.env.HOST,
-  user: process.env.USER,
+  user: "root",
   password: process.env.PASSWORD,
   database: process.env.DB
 });
--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
 echo "IP2021" | sudo -S cp /home/canvas/.env.example  /home/canvas/actions-runner/_work/flowup-canvas/flowup-canvas/.env
-cd /home/canvas/actions-runner/_work/flowup-canvas/flowup-canvas
-sed -i 's/USER.*/USER=root/' .env  
